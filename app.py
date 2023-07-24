b025172e8ea80ca885e723095967202082e5ac53
Taha Khan <brenda.colfel10@gmail.com>
2023-07-24 13:36:13 -0500

    Add files

---
+++ app.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+
+import streamlit as st
+from PIL import Image
+
+
+# --- PATH SETTINGS ---
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current_dir / "styles"/ "main.css"
+resume_file = current_dir / "assets" / "CV.pdf"
+profile_pic = current_dir / "assets" / "profile-pic.png"
+
+
+#--- GENERAL SETTINGS ---
+PAGE_TITLE = "Digital CV | Brenda Colorado Felix"
+PAGE_ICON = ":wave:"
+NAME = "Brenda Colorado Felix"
+DESCRIPTION = """
+Highly skilled Office Administrator with an impressive background specializing in administrative work and office support
+"""
+EMAIL = "Brenda.colfel10@gmail.com"
+SOCIAL_MEDIA = {
+    "LinkedIn": "https://www.linkedin.com/in/brenda-colorado-felix-b28413263/",
+    "Github": "https://github.com/Brendacolfel10",
+}
+
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
+
+#---LOAD CSS, PDF & PORFIL PIC ---
+with open(css_file) as f:
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+        PDFbyte = pdf_file.read()
+        profile_pic = Image.open(profile_pic)      
+
+
+# --- hero section ---
+col1, col2 = st.columns(2, gap="small")
+with col1:
+      st.image(profile_pic,caption="Support Engineer", width=230)
+
+with col2:
+      st.title(NAME)
+      st.write(DESCRIPTION)
+      st.download_button(
+            label=" 📄 Download Resume",
+            data=PDFbyte,
+            file_name=resume_file.name,
+            mime="application/octet-sistem",
+      )
+      st.write("📫", EMAIL)
+
+
+#---SOCIAL LINKS---
+st.write("#")
+cols = st.columns(len(SOCIAL_MEDIA))
+for index, (plataform, link) in enumerate(SOCIAL_MEDIA.items()):
+      cols[index].write(f"[{plataform}]({link})")
+
+
+#--- EXPERIENCE & QUALIFICATIONS ---
+st.write("#")
+st.subheader("Experience & Qualifications")
+st.write(
+      """
+ - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
+ - ✔️ Experience and knowledge in python and excel
+ - ✔️ Good understending of satisfacial principles and their respective applications
+ - ✔️ Experience extracting actionable insigths from data
+ - ✔️ Able to handle multiple customer inquiries simultaneously without compromising on the quality of support
+ - ✔️ Willingness to share knowledge and best practices within the support team for continuous improvement 
+ - ✔️ Strong desire to learn and stay updated with the latest technologies and industry trends
+ - ✔️ Experienced in conducting product training sessions to enhance customer understanding and self-sufficiency
+ - ✔️ Proficient in conflict resolution and maintaining professionalism during challenging situations
+"""
+)
+
+
+# --- SKILLS ---
+
+st.write("#")
+st.subheader("Hard Skills")
+st.write(
+      """
+ - 👩‍💻 Programing: python (Scikit-learn, Pandas), SQL, VBA  
+ - 📊 Data Visualization: powerBi, MS Excel, Plotly
+ - 📚 Modeling: Logistic regression, linear regression, decition trees
+ - 🗄️ Databases: Postgres, MongoDB, MySQL
+ - 🚧 Troubleshooting and issue resolution
+"""
+)
